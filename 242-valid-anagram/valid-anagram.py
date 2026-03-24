1c3c3063da95f8ff0c137b2d5f7b686dcc8d21d8
Sviat <ssoro4ak@gmail.com>
2026-03-23 20:27:17 -0400

    Time: 17 ms (20.29%) | Memory: 19.3 MB (87.81%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        container = {}
+        lenght = len(s)
+        
+        if(lenght != len(t)):
+            return False
+
+        for i in range(lenght):
+            if s[i] in container:
+                 container[s[i]] += 1
+            else:
+                container[s[i]] = 1
+
+        for i in range(lenght):
+
+            if t[i] in container and container[t[i]] > 0:
+                container[t[i]] -= 1
+            else:
+                return False
+        
+        return True
+           
+
