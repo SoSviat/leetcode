f8d7012ff7db087446fd2d9cc07fcc110f1f02b4
Sviat <ssoro4ak@gmail.com>
2026-04-18 08:35:19 -0400

    Time: 51 ms (47.11%) | Memory: 19.1 MB (91.37%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,9 +1,35 @@
 class Solution:
-    def fib(self, n: int) -> int:
-
-        if(n == 0 or n == 1):
+    def fib(self, n: int, dict_: list | None = None) -> int:
+        
+        #print('start')
+        #res = n 
+        if n == 0 or n == 1:
             return n
-                    
-        res = self.fib(n - 1) + self.fib(n - 2)
+            
+        if dict_ is None:
+            dict_ = []
+            dict_.append(0)
+            dict_.append(1)
+    
 
-        return res
+        if len(dict_) == n:
+            #print('win 1')
+            res = dict_[-1] + dict_[-2]
+            #print(res)
+            return res
+        else:
+            #print('append')
+            dict_.append(dict_[-1] + dict_[-2])
+            self.fib(n, dict_)
+
+        #print(dict_)
+
+        #print('final?', res)
+        return self.fib(n, dict_)
+        #return n
+
+        #if(n == 0 or n == 1):
+        #    return n
+        #            
+        #res = self.fib(n - 1, dict_) + self.fib(n - 2, dict_)
+        #return res
