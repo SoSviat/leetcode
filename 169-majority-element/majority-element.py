b213d27ccd2de04f74ae91e72333c1ccd08e2be6
Sviat <ssoro4ak@gmail.com>
2026-06-16 18:22:16 -0400

    Time: 3 ms (85.89%) | Memory: 21.1 MB (52.29%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,36 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        max_val = len(nums)//2
-        my_dict = {}
+        candidate = count = 0
 
-        for i in range(len(nums)):
-            if nums[i] in my_dict:
-                my_dict[nums[i]] += 1
-            else:
-                my_dict[nums[i]] = 1
+        for num in nums:
+            if count == 0:
+                candidate = num
+                
+            if candidate != num:
+                count -= 1
+            elif candidate == num:
+                count += 1 
+            
 
-            if my_dict[nums[i]] > max_val:
-                   return nums[i]
+        
+
+        print(candidate)
+        return candidate
+        
+        
+        # firtst trye
+        # max_val = len(nums)//2
+        # my_dict = {}
+
+        # for i in range(len(nums)):
+        #     if nums[i] in my_dict:
+        #         my_dict[nums[i]] += 1
+        #     else:
+        #         my_dict[nums[i]] = 1
+
+        #     if my_dict[nums[i]] > max_val:
+        #            return nums[i]
+
+        #0(n)
+        #0(n)
