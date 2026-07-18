f44777ff189af521ec1f063ac8b4979a8f4ab513
Sviat <ssoro4ak@gmail.com>
2026-07-18 05:20:35 +0300

    Time: 71 ms (60.75%) | Memory: 19.6 MB (87.70%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,25 +1,14 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         chart_hash = defaultdict(int)
-        #print(s)
 
         for c in s:
             chart_hash[c] += 1
         
-        i = 0
-        res = -1
-
-        for key, val in chart_hash.items():
-            #print(s[val])
-            if val == 1:
-
-                res = key
-                break
-            i += 1
-
-        for i in range(len(s)):
-            print(s[i],'- ', res)
-            if s[i] == res:
+        for i, v in enumerate(s):
+            if v in chart_hash and chart_hash[v] == 1:
                 return i
-        
-        return res
+        return -1
+
+        #O(n)
+        #O(1)
