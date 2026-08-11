2a612256423173abacb062e5403d1b70f83b4498
Sviat <ssoro4ak@gmail.com>
2026-08-11 23:47:16 +0300

    Time: 2 ms (37.18%) | Memory: 19.1 MB (90.99%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,27 +1,49 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
-        open_brackets = ["(","[","{"]
 
+        brackets = {
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        }
         res = []
         for i in range(len(s)):
-            print(s[i])
-            if s[i] in open_brackets:
-                res.append(s[i])
-            elif len(res) > 0:
-                if s[i] == ")" and res[-1] == "(":
-                    res.pop()
-                elif s[i] == "]" and res[-1] == "[":
-                    res.pop()
-                elif s[i] == "}" and res[-1] == "{":
+            if s[i] in brackets:
+                if res and res[-1] == brackets[s[i]]:
                     res.pop()
                 else:
                     return False
             else:
-                return False
-            
+                res.append(s[i])
         
         if len(res) > 0:
             return False
         
         return True
+
+        # O(n)
+        # O(n)
+        # open_brackets = ["(","[","{"]
+
+        # res = []
+        # for i in range(len(s)):
+        #     print(s[i])
+        #     if s[i] in open_brackets:
+        #         res.append(s[i])
+        #     elif len(res) > 0:
+        #         if s[i] == ")" and res[-1] == "(":
+        #             res.pop()
+        #         elif s[i] == "]" and res[-1] == "[":
+        #             res.pop()
+        #         elif s[i] == "}" and res[-1] == "{":
+        #             res.pop()
+        #         else:
+        #             return False
+        #     else:
+        #         return False
+            
+        
+        # if len(res) > 0:
+        #     return False
+        
+        # return True
